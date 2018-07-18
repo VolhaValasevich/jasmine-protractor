d0ea6425245a8ee9b06ad55e12ebce803ee3c2d9
Volha Valasevich <volha_valasevich@epam.com>
2018-07-18 11:15:57 +0300

    add first tests

--- conf.js
+++ conf.js
@@ -1,8 +1,14 @@
 // conf.js
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+
   specs: ['spec.js'],
+
   capabilities: {
     browserName: 'chrome'
+  },
+
+  onPrepare: () => {
+    browser.driver.manage().window().maximize();
   }
 }
--- spec.js
+++ spec.js
@@ -1,52 +1,34 @@
 // spec.js
-describe('angularjs homepage', function () {
-    var firstNumber = element(by.model('first'));
-    var secondNumber = element(by.model('second'));
-    var goButton = element(by.id('gobutton'));
-    var latestResult = element(by.binding('latest'));
-    var history = element.all(by.repeater('result in memory'));
 
-    function add(a, b) {
-        firstNumber.sendKeys(a);
-        secondNumber.sendKeys(b);
-        goButton.click();
-    }
+function getStartPage() {
+        browser.get("https://www.t-mobile.com/");
+}
 
-    beforeEach(function () {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+function clickLink(linktext) {
+    element(by.linkText(linktext)).click();
+}
+
+xdescribe('T-Mobile plan page', () => {
+
+    beforeEach(() => {
+        getStartPage();
+        clickLink("PLANS");
     });
 
-    it('should have a history', function () {
-        add(1, 2);
-        add(3, 4);
-
-        expect(history.count()).toEqual(2);
-
-        add(5, 6);
-
-        expect(history.count()).toEqual(3);
-
-        expect(history.last().getText()).toContain('1 + 2');
-        expect(history.first().getText()).toContain('5 + 6');
-    });
-
-    it('should have a title', function () {
-        expect(browser.getTitle()).toEqual('Super Calculator');
-    });
-
-    it('should add one and two', function () {
-        firstNumber.sendKeys(1);
-        secondNumber.sendKeys(2);
-
-        goButton.click();
-
-        expect(latestResult.getText()).toEqual('3');
-    });
-
-    it('should add four and six', function () {
-        firstNumber.sendKeys('4');
-        secondNumber.sendKeys('6');
-        goButton.click();
-        expect(latestResult.getText()).toEqual('10');
+    it('should have the right title', () => {
+        expect(browser.getTitle()).toEqual("Cell Phone Plans | Family Plans | Compare Cell Phone Plans | T-Mobile");
     });
 });
+
+describe('T-Mobile phones page', () => {
+
+    beforeEach(() => {
+        getStartPage();
+        clickLink("PHONES");
+    });
+
+    it('should have the right title', () => {
+        expect(browser.getTitle()).toEqual("Smartphones & Cell Phones | Compare our Best Cell Phones & Smartphones");
+    });
+
+})
