066a7a56c90685711ee28e5d1a0865c6183e02f3
Volha Valasevich <volha_valasevich@epam.com>
2018-07-30 11:59:14 +0300

    change log levels

--- util/jasmine-custom-reporter.js
+++ util/jasmine-custom-reporter.js
@@ -3,33 +3,33 @@ const logger = require("./logger.js").logger;
 class myReporter {
 
     jasmineStarted (suiteInfo) {
-        logger.warn('Running suite with ' + suiteInfo.totalSpecsDefined + ' specs');
+        logger.start('Running suite with ' + suiteInfo.totalSpecsDefined + ' specs');
     }
 
     suiteStarted (result) {
-        logger.warn('Suite started: ' + result.description);
+        logger.start('Suite started: ' + result.description);
     }
 
     specStarted (result) {
-        logger.warn('Spec started: ' + result.description);
+        logger.start('Spec started: ' + result.description);
     }
 
     specDone(result) {
-        logger.verbose('Spec: ' + result.description + ' was ' + result.status);
+        logger.finish('Spec: ' + result.description + ' was ' + result.status);
         for (let i = 0; i < result.failedExpectations.length; i++) {
-            logger.error('Failure: ' + result.failedExpectations[i].message);
+            logger.fail('Failure: ' + result.failedExpectations[i].message);
         }
     }
 
     suiteDone (result) {
-        logger.verbose('Suite: ' + result.description + ' was ' + result.status);
+        logger.finish('Suite: ' + result.description + ' was ' + result.status);
         for (let i = 0; i < result.failedExpectations.length; i++) {
-            logger.error('AfterAll ' + result.failedExpectations[i].message);
+            logger.fail('AfterAll ' + result.failedExpectations[i].message);
         }
     }
 
     jasmineDone() {
-        logger.warn('Finished suite');
+        logger.finish('Finished suite');
     }
 }
 
--- util/logger.js
+++ util/logger.js
@@ -1,8 +1,26 @@
 const winston = require("winston");
 
+const myCustomLevels = {
+    levels: {
+        fail: 0,
+        start: 1,
+        info: 2,
+        finish: 3
+    },
+    colors: {
+        fail: 'red',
+        start: 'yellow',
+        info: 'white',
+        finish: 'green'
+    }
+};
+
+winston.addColors(myCustomLevels.colors);
+
 class Logger {
     constructor() {
         this.logger = winston.createLogger({
+            levels: myCustomLevels.levels,
             transports: [
                 new winston.transports.Console({
                     format: winston.format.combine(
@@ -11,7 +29,7 @@ class Logger {
                         }),
                         winston.format.simple()
                     ),
-                    level: "debug"
+                    level: "finish"
                 }),
                 new winston.transports.File({filename: 'combined.log'})
             ]
